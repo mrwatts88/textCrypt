39123e4e58c29f4844e9ebf3f1f6e2f95d065141
Matt Watts <matthew.watts.mw@gmail.com>
2018-05-27 19:09:11 -0500

    add git ignore

---
+++ cmake-build-debug/CMakeFiles/textCrypt.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/textCrypt.dir/main.cpp.o"
+  "textCrypt.pdb"
+  "textCrypt"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/textCrypt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
